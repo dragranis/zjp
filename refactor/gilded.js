47774c41ff178dc24c5443f1b38524966ec762e5
dragranis <dragfirm2@gmail.com>
2019-11-06 11:53:55 +0100

    Update gilded.js

--- refactor/gilded.js
+++ refactor/gilded.js
@@ -1,104 +1,71 @@
 class Item {
-    constructor(name, sellIn, quality){
-      this.name = name;
-      this.sellIn = sellIn;
-      this.quality = quality;
-    }
+  constructor(name, sellIn, quality) {
+    this.name = name;
+    this.sellIn = sellIn;
+    this.quality = quality;
   }
 
-
-  
-  var shop_items = [
-      new Item("+5 Dexterity Vest",10,20),
-      new Item("Aged Brie",2,0),
-      new Item("Elixir of the Mongoose",5,7),
-      new Item("Sulfuras, Hand of Ragnaros",0,80),
-      new Item("Sulfuras, Hand of Ragnaros",-1,80),
-      new Item("Backstage passes to a TAFKAL80ETC concert",15,20),
-      new Item("Backstage passes to a TAFKAL80ETC concert",10,49),
-      new Item("Backstage passes to a TAFKAL80ETC concert",5,49),
-      new Item("Conjured Mana Cake",12,50)
-  ]
-  
-  
-  class Shop {
-    constructor(items=[]){
-      this.items = items;
-    }
-      
-     backstage_value(product){
-         if(product.sellIn>10) return -1;
-         if(product.sellIn<=10)   return -2;
-         if(product.sellIn<=5) return -3;
-         if(product.sellIn<0) return 0;
-     }
-      
-     decrease_days(product){
-        (this.is_legendary(product)) ? null : product.sellIn--;
-     }
-      
-    changed_by(product){
-        let value = 1;
-        if(this.days_passed(product)) value++;
-        if(this.is_increasing_quality(product)) value*=-1;
-        if(this.is_backstage(product)){
-            value = this.backstage_value(product);
-            console.log(value);
-            
-        }
-        if(this.is_conjured(product)) value*=2;
-        if(this.is_legendary(product)) value = 0;
-        return value;
-    }
-    
-    days_passed(product){
-        return product.sellIn && product.sellIn<0;      
-    }
-      
-    change_quality(product){
-        product.quality-=this.changed_by(product);
-    }
-      
-    is_negative(product){
-       (product.quality<0) ? product.quality = 0 : null;   
-    }
-    
-    is_maximum(product){
-        (product.quality<=50) ? null : product.quality=50;
-    }
-      
-    is_backstage(product){
-        return product.name.includes("Backstage");   
-    }
-      
-    is_legendary(product){
-        return product.name.includes("Sulfuras");   
-    }
-      
-    is_conjured(product){
-        return product.name.includes("Conjured");
-    }
-    
-    is_increasing_quality(product){
-        return product.name.includes("Brie") || product.name.includes("Backstage");
-    }
-      
-      
-     update(days){
-        for(var i=1;i<=days;i++){
-            document.writeln("<br/>Day: "+i+"<br/>");
-            document.writeln("Name, sellIn, Quality<br/>");
-            for(var j in this.items){
-               document.writeln(this.items[j].name+","+this.items[j].sellIn+","+this.items[j].quality+"<br/>");
-                this.change_quality(this.items[j]);
-                this.decrease_days(this.items[j]);
-                this.is_maximum(this.items[j]);
-                this.is_negative(this.items[j]);
-            }
-        }
-     }
+  backstage_decrease() {
+    if (this.sellIn > 10) return -1;
+    if (this.sellIn >= 5 && this.sellIn <= 10) return -2;
+    if (this.sellIn >= 0 && this.sellIn <= 4) return -3;
+    else return (this.quality *= -1);
   }
-  
-  
-  
-  var sklep = new Shop(shop_items);
+
+  changed_quality() {
+    let value = 1;
+    if (this.is_legendary()) value = 0;
+    if (this.is_backstage()) value = this.backstage_decrease();
+    if (this.is_increasing_quality()) value *= -1;
+    if (this.past_sellIn()) value *= 2;
+    if (this.is_conjured()) value *= 2;
+  }
+
+  decrease_days() {
+    this.sellIn--;
+  }
+
+  is_backstage() {
+    return this.name.includes("Backstage");
+  }
+
+  is_conjured() {
+    return this.name.includes("Conjured");
+  }
+
+  is_increasing_quality() {
+    return this.name.includes("Brie");
+  }
+
+  is_legendary() {
+    return this.name.includes("Sulfuras");
+  }
+
+  minimum_quality() {
+    this.quality < 0 ? (this.quality = 0) : null;
+  }
+
+  maximum_quality() {
+    this.quality > 50 ? (this.quality = 50) : null;
+  }
+
+  past_sellIn() {
+    return this.sellIn && this.sellIn < 0;
+  }
+
+  write() {
+    document.writeln(this.name + "," + this.sellIn + "," + this.quality);
+  }
+}
+
+var shop_items = [
+  new Item("+5 Dexterity Vest", 10, 20),
+  new Item("Aged Brie", 2, 0),
+  new Item("Elixir of the Mongoose", 5, 7),
+  new Item("Sulfuras, Hand of Ragnaros", 0, 80),
+  new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+  new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+  new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+  new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+  new Item("Conjured Mana Cake", 12, 50)
+];
